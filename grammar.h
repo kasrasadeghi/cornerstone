368a34cc5324c0570679184891c3a0abfc99e251
kasrasadeghi <kasra.sadeghi1@gmail.com>
2019-01-13 03:11:07 -0600

    move type enum to type.h

--- grammar.h
+++ grammar.h
@@ -1,66 +1,10 @@
 #pragma once
 #include "texp.h"
+#include "type.h"
 #include <functional>
 #include <unordered_map>
 
 namespace Typing {
-
-  
-enum class Type : size_t
-  {
-    Program,
-      TopLevel,
-      StrTable,
-        StrTableEntry,
-      Struct,
-        Field,
-      Def,
-      Decl,
-    
-    Stmt,
-      If,
-      Store,
-      Auto,
-      Do,
-      Return,
-        ReturnExpr,
-        ReturnVoid,
-      Let,
-    
-    Value,
-      StrGet,
-      Literal,
-        IntLiteral,
-        BoolLiteral,
-      String,
-      Name,
-    
-    Types,
-      Type,
-    
-    Params,
-      Param,
-    
-    Expr,
-      MathBinop,
-      Icmp,
-        LT,
-        LE,
-        GT,
-        GE,
-        EQ,
-        NE,
-      Load,
-      Index,
-      Cast,
-      Add,
-
-    Call,
-      CallBasic,
-      CallVargs,
-      CallTail,
-      Args,
-  };
 
 bool is(Type type, const Texp& t, bool trace = true);
 }
---
+++ type.h
@@ -0,0 +1,64 @@
+#pragma once
+#include <stddef.h>
+#include <string_view>
+#include "macros.h"
+
+namespace Typing {
+
+enum class Type : size_t
+  {
+    Program,
+      TopLevel,
+      StrTable,
+        StrTableEntry,
+      Struct,
+        Field,
+      Def,
+      Decl,
+    
+    Stmt,
+      If,
+      Store,
+      Auto,
+      Do,
+      Return,
+        ReturnExpr,
+        ReturnVoid,
+      Let,
+    
+    Value,
+      StrGet,
+      Literal,
+        IntLiteral,
+        BoolLiteral,
+      String,
+      Name,
+    
+    Types,
+      Type,
+    
+    Params,
+      Param,
+    
+    Expr,
+      MathBinop,
+      Icmp,
+        LT,
+        LE,
+        GT,
+        GE,
+        EQ,
+        NE,
+      Load,
+      Index,
+      Cast,
+      Add,
+
+    Call,
+      CallBasic,
+      CallVargs,
+      CallTail,
+      Args,
+  };
+
+}
