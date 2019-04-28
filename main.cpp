c620001fb14eeec40ae33bcc6775db8b3470fae5
kasrasadeghi <kasra.sadeghi1@gmail.com>
2019-04-27 19:34:38 -0500

    remove test cases from main

--- main.cpp
+++ main.cpp
@@ -34,49 +34,7 @@ void stdin_main()
     std::cout << result << std::endl;
   }
 
-void return_void_empty_test()
-  {
-    using namespace Typing;
-    std::cout << is(Type::Return, Parser::parseTexp("(return-void)")) << std::endl;
-    std::cout << is(Type::Return, Parser::parseTexp("(return-void 5)")) << std::endl;
-  }
-
-void string_parsing_test() 
-  {
-    Texp t = Parser::parseTexp(R"( (0 "Hello World!\00") )");
-    std::cout << t << std::endl;
-    std::cout << Typing::is(Typing::Type::StrTableEntry, t) << std::endl;
-    std::cout << t.tabs() << std::endl;
-  }
-
-void str_table_test()
-  {
-    Texp t = Parser::parseTexp(R"((0 "Hello World\00"))");
-    std::cout << t << std::endl;
-    std::cout << Typing::is(Typing::Type::StrTableEntry, t) << std::endl;
-    std::cout << t.tabs() << std::endl;
-  }
-
-void let_call_test()
-  {
-    Texp t = Parser::parseTexp("(let ignored (call puts (types i8*) i32 (args (str-get 0))))");
-    std::cout << t << std::endl;
-    std::cout << t.tabs() << std::endl;
-    std::cout << Typing::is(Typing::Type::Let, t) << std::endl;
-  }
-
-void field_test()
-  {
-    Texp t = Parser::parseTexp("(a i32)");
-    std::cout << t << std::endl;
-    std::cout << t.tabs() << std::endl;
-    std::cout << Typing::is(Typing::Type::Field, t) << std::endl;
-  }
-
 int main()
   { 
     stdin_main();
-
-    // let_call_test();
-    // field_test();
   }
