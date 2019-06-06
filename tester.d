f2a2b0ac5fd95c321a7879aeaa788a92ac4c012e
kasrasadeghi <kasra.sadeghi1@gmail.com>
2019-06-06 08:58:43 -0500

    add shell tester

--- tester.d
+++ tester.d
@@ -3,8 +3,9 @@ module tester;
 import std.process;
 import std.stdio;
 
+/// runs a shell command and returns the exit code
 int run(string s) {
-  auto pipes = pipeProcess(s, Redirect.stdout);
+  auto pipes = pipeShell(s, Redirect.stdout);
   scope(exit) wait(pipes.pid);
   while(! tryWait(pipes.pid).terminated) {
     write(pipes.stdout.readln);
@@ -13,5 +14,7 @@ int run(string s) {
 }
 
 void main() {
-  run("make");
+  run("make build");
+  string filename = "argcall.bb";
+  run("build/main/cornerstone-cpp < ../backbone-test/backbone/" ~ filename);
 }
