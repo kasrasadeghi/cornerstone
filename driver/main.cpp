ccef91f5d1e18d994970656c7891c9f38dc2c8fa
kasrasadeghi <kasra.sadeghi1@gmail.com>
2019-07-16 00:17:11 -0500

    simplify main.cpp

--- driver/main.cpp
+++ driver/main.cpp
@@ -10,47 +10,21 @@
 #include <sstream>
 #include <fstream>
 
-void stdin_main()
+int main(int argc, char* argv[])
   {
-    auto parse_tree = parse();
-    Grammar g (parse_from_file("docs/bb-grammar.texp")[0]);
-    Matcher m {g};
-    if (auto proof = m.is(parse_tree, "Program"))
+    Texp program = (argc == 1) ? parse() : parse_from_file(argv[2]);
+
+    Grammar bb_g (parse_from_file("docs/bb-grammar.texp")[0]);
+    Matcher bb_m {bb_g};
+
+    if (auto proof = bb_m.is(program, "Program"))
       {
-        generate(g, parse_tree, *proof);
+        generate(bb_g, program, *proof);
         std::cout << "; " << *proof << std::endl;
       }
     else
-      std::cout << "grammar error" << std::endl;
-  }
-
-void file_main(int argc, char* argv[])
-  {
-    Grammar g (parse_from_file("docs/bb-grammar.texp")[0]);
-    Matcher m {g};
-
-    // parse files from argv
-    for (int i = 1; i < argc; ++i)
       {
-        Texp prog = parse_from_file(argv[i]);
-
-        if (auto proof = m.is(prog, "Program"))
-          {
-            generate(g, prog, *proof);
-            std::cout << "; " << *proof << std::endl;
-          }
-        else
-          {
-            std::cout << "grammar error with file: '" << argv[i] << "'" << std::endl;
-            exit(1);
-          }
+        std::cout << "grammar error with file: '" << (argc == 1 ? "STDIN" : argv[2]) << "'" << std::endl;
+        exit(1);
       }
-  }
-
-int main(int argc, char* argv[])
-  {
-    if (argc == 1)
-      stdin_main();
-    else
-      file_main(argc, argv);
   }
