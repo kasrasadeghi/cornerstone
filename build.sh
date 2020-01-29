1b85452ab07e3d2f74e20a31a57bb6aa13a59499
kasrasadeghi <kasra.sadeghi1@gmail.com>
2020-01-28 20:51:03 -0600

    build.sh works from any folder

--- build.sh
+++ build.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 if [ "$#" -ne 1 ]; then
     echo "usage: ./build.sh <filepath>"
-    echo "    where <filepath>.bb exists to create `basename <filepath>` executable in current working directory"
+    echo '    where <filepath>.bb exists to create `basename <filepath>` executable in current working directory'
+    exit 1
 fi
 
-../cornerstone-cpp/build/driver/cornerstone $1.bb | clang -xir -o `basename $1` -
+# build.sh is expected to be within the cornerstone directory
+# dirname to figure out relative path to cornerstone directory
+#   `dirname $0` -> path to cornerstone
+# cornerstone-cpp is expected to be the sibling of the cornerstone directory
+#   `dirname $0`/../cornerstone-cpp
+# the initial backbone file is expected to be within the current working directory
+#   `pwd`
+# basename to create the executable in the current directory
+`dirname $0`/../cornerstone-cpp/build/driver/cornerstone `pwd`/$1.bb | clang -xir -o `basename $1` -
+
