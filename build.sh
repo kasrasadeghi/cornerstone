3c895fc0db56c16b829039d8add34688e8a27efe
kasrasadeghi <kasra.sadeghi1@gmail.com>
2020-01-29 11:23:29 -0600

    add documentation to build.sh and -Wno-override-module

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+
+# if argc != 2
 if [ "$#" -ne 1 ]; then
     echo "usage: ./build.sh <filepath>"
     echo '    where <filepath>.bb exists to create `basename <filepath>` executable in current working directory'
     exit 1
 fi
 
+# TODO make less bad
 # build.sh is expected to be within the cornerstone directory
 # dirname to figure out relative path to cornerstone directory
 #   `dirname $0` -> path to cornerstone
@@ -13,5 +16,5 @@ fi
 # the initial backbone file is expected to be within the current working directory
 #   `pwd`
 # basename to create the executable in the current directory
-`dirname $0`/../cornerstone-cpp/build/driver/cornerstone `pwd`/$1.bb | clang -xir -o `basename $1` -
+`dirname $0`/../cornerstone-cpp/build/driver/cornerstone `pwd`/$1.bb | clang -xir -Wno-override-module -o `basename $1` -
 
